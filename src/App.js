40ba09dd25d0678d7dce70c6943df49d6069a429
unknown <elmiwahyutriyanice@gmail.com>
2023-11-09 21:51:39 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,364 @@
+import React from "react";
+import "./App.css"; // Adjust the path if needed
+
+function App() {
+  return (
+    <div>
+      <h2>Teka Teki Silang</h2>
+      <div className="main-container">
+        <div className="left">
+          <table className="table1">
+            <tr>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white1" id="W1L1">
+                <span class="kata">1</span>
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W1L2">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W1L3">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W1L4">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W1L5">
+                <span class="kata">2</span>
+                <input type="text" maxlength="1" />
+              </td>
+            </tr>
+            <tr>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white1" id="W10L2">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white1" id="W2L2">
+                <input type="text" maxlength="1" />
+              </td>
+            </tr>
+            <tr>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white1" id="W10L3">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white1" id="W2L3">
+                <input type="text" maxlength="1" />
+              </td>
+            </tr>
+            <tr>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white1" id="W3L1">
+                <span class="kata">3</span>
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W10L4">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W3L3">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W3L4">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W3L5">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W2L4">
+                <input type="text" maxlength="1" />
+              </td>
+            </tr>
+            <tr>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white1" id="W10L5">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white1" id="W2L5">
+                <input type="text" maxlength="1" />
+              </td>
+            </tr>
+
+            <tr>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white1" id="W10L6">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white1" id="W2L6">
+                <input type="text" maxlength="1" />
+              </td>
+            </tr>
+            <tr>
+              <td class="white2"></td>
+              <td class="white1" id="W4L1">
+                <span class="kata">4</span>
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white1" id="W5L1">
+                <span class="kata">5</span>
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white1" id="W6L1">
+                <span class="kata">6</span>
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W6L2">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W6L3">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W10L7">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W6L4">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+            </tr>
+            <tr>
+              <td class="white2"></td>
+              <td class="white1" id="W4L2">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white1" id="W5L2">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white1" id="W9L2">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white1" id="W10L8">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+            </tr>
+            <tr>
+              <td class="white1" id="W7L1">
+                <span class="kata">7</span>
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W4L3">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W7L3">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W5L3">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W7L5">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W9L3">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W7L7">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+            </tr>
+            <tr>
+              <td class="white2"></td>
+              <td class="white1" id="W4L4">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white1" id="W5L4">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white1" id="W9L4">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+            </tr>
+
+            <tr>
+              <td class="white2"></td>
+              <td class="white1" id="W4L5">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+            </tr>
+
+            <tr>
+              <td class="white2"></td>
+              <td class="white1" id="W4L6">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+            </tr>
+
+            <tr>
+              <td class="white1" id="W8L1">
+                <span class="kata">8</span>
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W4L7">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white1" id="W8L3">
+                <input type="text" maxlength="1" />
+              </td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+              <td class="white2"></td>
+            </tr>
+          </table>
+        </div>
+
+        <div className="right">
+          <div className="clue">
+            <ul>
+              <li>
+                <span className="tipe">Menurun</span>
+                <div>
+                  <span className="number">1)</span>
+                  Halaman utama suatu situs web.
+                </div>
+                <div>
+                  <span class="number">2)</span>
+                  Tempat untuk mencari informasi di internet.
+                </div>
+                <div>
+                  <span class="number">4)</span>
+                  Sejenis program untuk menjelajah internet.
+                </div>
+                <div>
+                  <span class="number">5)</span>
+                  File yang mendefinisikan tampilan situs web.
+                </div>
+                <div>
+                  <span class="number">6)</span>
+                  Berfungsi untuk menghubungkan halaman situs web.
+                </div>
+              </li>
+              <li>
+                <span className="tipe">Mendatar</span>
+                <div>
+                  <span className="number">1)</span>
+                  Singkatan dari protokol keamanan internet.
+                </div>
+                <div>
+                  <span class="number">3)</span>
+                  Tempat penyimpanan berkas-berkas situs web.
+                </div>
+                <div>
+                  <span class="number">6)</span>
+                  Tempat untuk mendaftar dan masuk ke situs web.
+                </div>
+                <div>
+                  <span class="number">7)</span>
+                  Tempat untuk memberikan komentar di situs web
+                </div>
+                <div>
+                  <span class="number">8)</span>
+                  Singkatan dari alamat situs web.
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          <div className="info">
+            <ul>
+              <li>Klik kotak lalu ketik sebuah huruf</li>
+              <li>Gunakan huruf kapital</li>
+              <li>jika BENAR maka kotak berwarna MERAH</li>
+              <li>jika SALAH maka kotak berwarna HIJAU</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
